59b529fbfec861b0447607b59c8daf4185980ffd
RenanOliveiraSilva <renandeoliveirasilva123@gmail.com>
2024-05-28 20:56:28 -0300

    Desenv

---
+++ login/script.js
@@ -0,0 +1,33 @@
+
+
+document.addEventListener('submit', e => {
+    const inputT = document.getElementById('user').value;
+    const inputP = document.getElementById('pass').value;
+    const btn = document.getElementById('bt')
+    
+    e.preventDefault();
+
+    valida(inputT, inputP);
+
+});
+
+function valida(user, pass) {
+    if(user === "ADM" && pass === "123") {
+        alert("ADM logado")
+        return
+    }
+
+    if(user === "Cliente" && pass === "123") {
+        alert("Cliente logado")
+        return
+    }
+
+    if(user === "Vet" && pass === "123") {
+        alert("Veterinário logado")
+        return
+    }
+
+    alert("Usuários ou Senhas incorretas")
+    return
+
+}
