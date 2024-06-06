65621a130312b8a4f98640c1cafbdd437e4e9023
RenanOliveiraSilva <renandeoliveirasilva123@gmail.com>
2024-06-06 19:23:55 -0300

    modified:   login/script.js 	modified:   vet/assets/css/layout.css 	renamed:    vet/layout.html -> vet/home.html 	modified:   vet/pages/realizaConsulta/realizaConsultas.html 	new file:   vet/pages/t.txt

--- login/script.js
+++ login/script.js
@@ -14,21 +14,25 @@ document.addEventListener('submit', e => {
 function valida(user, pass) {
     if(user === "ADM" && pass === "123") {
         alert("ADM logado")
+        window.location.href = '../adm/home.html';
         return
     }
 
     if(user === "Cliente" && pass === "123") {
         alert("Cliente logado")
+        window.location.href = '../cli/home.html';
         return
     }
 
     if(user === "Vet" && pass === "123") {
         alert("Veterinário logado")
+        window.location.href = '../vet/home.html';
         return
     }
 
     if(user === "Secretaria" && pass === "123") {
         alert("Veterinário logado")
+        window.location.href = '../sec/home.html';
         return
     }
 
